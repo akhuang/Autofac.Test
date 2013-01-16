e2bcf375217689b6035cd5274d88d6728a342fd9
phoenix <fy24h.huang@gmail.com>
2013-01-16 17:04:17 +0800

    add auotfac nunit test

---
+++ Autofac_Ex01/Autofac_Ex01/ContainerBuilderTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac;
+
+namespace Autofac_Ex01
+{
+    [TestFixture]
+    public class ContainerBuilderTest
+    {
+        interface IA { };
+
+        interface IB { };
+
+        public class AB : IA, IB
+        {
+        }
+
+        [Test]
+        public void SimpleReg()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AB>();
+
+            var container = builder.Build();
+            var a = container.Resolve<AB>();
+            Assert.IsNotNull(a);
+        }
+
+    }
+}
---
+++ Autofac_Ex01/Autofac_Ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autofac_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
