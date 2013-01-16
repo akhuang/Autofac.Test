4bd07e0e5a88e25f013b794fc103a98ee73dc2c7
phoenix <fy24h.huang@gmail.com>
2013-01-16 23:28:33 +0800

    update

--- Autofac_Ex01/Autofac_Ex01/ContainerBuilderTest.cs
+++ Autofac_Ex01/Autofac_Ex01/ContainerBuilderTest.cs
@@ -14,8 +14,9 @@ namespace Autofac_Ex01
 
         interface IB { };
 
-        public class AB : IA, IB
+        public class AB : DisposeTracker, IA, IB
         {
+            public void Do() { }
         }
 
         [Test]
@@ -23,11 +24,128 @@ namespace Autofac_Ex01
         {
             var builder = new ContainerBuilder();
             builder.RegisterType<AB>();
-
             var container = builder.Build();
             var a = container.Resolve<AB>();
             Assert.IsNotNull(a);
+            Assert.IsInstanceOf<AB>(a);
         }
 
+        [Test]
+        public void SimpleRegInterface()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AB>().As<IA>();
+            var container = builder.Build();
+            var a = container.Resolve<IA>();
+
+            Assert.IsNotNull(a);
+            Assert.IsInstanceOf<AB>(a);
+            Assert.IsFalse(container.IsRegistered<AB>());
+        }
+
+        [Test]
+        public void WithExternalFactory()
+        {
+            //在container回收后，a1,a2内存不会被回收
+            //用a1,a2是两个不同的实例
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AB>().As<IA>().ExternallyOwned();
+            var container = builder.Build();
+            var a1 = container.Resolve<IA>();
+            var a2 = container.Resolve<IA>();
+
+            container.Dispose();
+
+            Assert.IsNotNull(a1);
+            Assert.AreNotEqual(a1, a2);
+            Assert.IsFalse(((AB)a1).IsDisposed);
+        }
+
+        [Test]
+        public void WithInternalSingleton()
+        {
+            //在container回收后，a1,a2内存也被回收
+            //用a1,a2是两个相同的实例
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AB>()
+                .As<IA>()
+                .OwnedByLifetimeScope() //实例跟随container一起dispose;默认
+                .SingleInstance();
+
+            var container = builder.Build();
+            var a1 = container.Resolve<IA>();
+            var a2 = container.Resolve<IA>();
+
+            container.Dispose();
+
+            Assert.IsNotNull(a1);
+            Assert.AreSame(a1, a2);
+            Assert.IsTrue(((AB)a1).IsDisposed);
+        }
+
+        [Test]
+        public void WithFactoryContext()
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<AB>().As<IA>();
+
+            var container = cb.Build();
+            var ctx = container.BeginLifetimeScope();
+            var a1 = ctx.Resolve<IA>();
+            var a2 = ctx.Resolve<IA>();
+            ctx.Dispose();
+
+            Assert.IsNotNull(a1);
+            Assert.AreNotSame(a1, a2);
+            Assert.IsTrue(((AB)a1).IsDisposed);
+
+        }
+
+        [Test]
+        public void RegistrationOrderingPreserved()
+        {
+            //先进后出
+            var target = new ContainerBuilder();
+            var inst1 = new object();
+            var inst2 = new object();
+
+            target.RegisterInstance(inst1);
+            target.RegisterInstance(inst2);
+
+            Assert.AreSame(inst2, target.Build().Resolve<object>());
+        }
+
+        class ObjectModule : Module
+        {
+            public bool ConfigureCalled { get; private set; }
+
+            protected override void Load(ContainerBuilder builder)
+            {
+                if (builder == null)
+                    throw new ArgumentNullException("builder");
+
+                ConfigureCalled = true;
+                builder.RegisterType<object>().SingleInstance();
+            }
+        }
+
+        [Test]
+        public void RegisterModule()
+        {
+            //注册模块
+            //builder之前注册是没有生效的
+
+            var mod = new ObjectModule();
+            var target = new ContainerBuilder();
+            target.RegisterModule(mod);
+
+            Assert.IsFalse(mod.ConfigureCalled);
+
+            var container = target.Build();
+            Assert.IsTrue(mod.ConfigureCalled);
+            Assert.IsTrue(container.IsRegistered<object>());
+        }
     }
 }
---
+++ Autofac_Ex01/Autofac_Ex01/DisposeTracker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autofac_Ex01
+{
+    public class DisposeTracker : IDisposable
+    {
+        public event EventHandler<EventArgs> Disposing;
+
+        public bool IsDisposed;
+
+        public void Dispose()
+        {
+            IsDisposed = true; 
+
+            if (Disposing != null)
+                Disposing(this, EventArgs.Empty);
+        }
+    }
+}
