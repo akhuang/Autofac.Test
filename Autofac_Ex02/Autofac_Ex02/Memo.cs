7a3bd6b27e3dd4397fb491c959dedd992c6bf747
phoenix <fy24h.huang@gmail.com>
2013-04-23 23:28:26 +0800

    update

---
+++ Autofac_Ex02/Autofac_Ex02/Memo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autofac_Ex02
+{
+    public class Memo
+    {
+        public string Title { get; set; }
+        public DateTime DueDt { get; set; }
+    }
+}
---
+++ Autofac_Ex02/Autofac_Ex02/Program.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autofac_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQueryable<Memo> memos = new List<Memo>()
+            {
+                new Memo{ Title="aaa",DueDt=DateTime.Now},
+                new Memo{ Title="bbbb",DueDt=new DateTime(2007,2,2)}
+            }.AsQueryable();
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<IMemoDueNotifier>().As<PrintingNotifier>();
+
+            var memoChecker = new MemoChecker(memos, new PrintingNotifier(Console.Out));
+            memoChecker.CheckNow();
+            Console.ReadKey();
+
+        }
+    }
+}
