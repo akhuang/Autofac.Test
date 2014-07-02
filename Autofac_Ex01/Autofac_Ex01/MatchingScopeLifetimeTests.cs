0389024381050cdaed98b6e0cb774096557b2a39
phoenix <fy24h.huang@gmail.com>
2014-07-02 11:58:53 +0800

    add InstancePerMatchingLifetimeScope test

---
+++ Autofac_Ex01/Autofac_Ex01/MatchingScopeLifetimeTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac;
+using Autofac.Core;
+using Autofac.TestAssembly;
+
+namespace Autofac_Ex01
+{
+
+    [TestFixture]
+    public class MatchingScopeLifetimeTests
+    {
+        interface IA { };
+
+        interface IB { };
+
+        public class AB : DisposeTracker, IA, IB
+        {
+            public void Do() { }
+        }
+
+
+        [Test]
+        public void InstancePerMatchingLifetimeScope()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AB>().As<IA>().InstancePerMatchingLifetimeScope("shell");
+            var container = builder.Build();
+
+            var ex = Assert.Throws<DependencyResolutionException>(() => container.Resolve<IA>());
+            Assert.IsNotNull(ex);
+            Assert.That(ex.Message.Contains("shell"));
+
+            //var a = container.Resolve<IA>();
+
+            var scope = container.BeginLifetimeScope("shell");
+            var scopeInstance = scope.Resolve<IA>();
+
+            //Assert.IsNull(a);
+            Assert.IsNotNull(scopeInstance);
+        }
+    }
+}
